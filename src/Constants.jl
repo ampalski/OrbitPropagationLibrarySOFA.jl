91465ba790b622ead0da78ed393fb057434e992d
Andrew Palski <ampalski@gmail.com>
2024-05-29 09:17:12 -0600

    starting dat

--- src/Constants.jl
+++ src/Constants.jl
@@ -20,3 +20,25 @@ const TTMTAI = 32.184
 const AU = 149597870.7 #km
 const CMPS = 299792458.0 #m/s
 
+"""
+    ΔAT = dat(dateVec)
+
+Find the TAI-UTC value (ΔAT) from a date vector
+
+A date vector is a length 6 vector of floats with
+[year, month, day, hour, minute, seconds]
+
+    TODO: Finish this description. based on iauDat.c
+The return values are Julian Date returned in two pieces, in the usual SOFA
+manner, which is designed to preserve time resolution. The full Julian Date is
+available as a single number by adding the two components of the vector.
+
+Specifically, Julian Date contains the full number of days in JD[1] and the day
+fraction in JD[2].
+
+Derived from SOFA's `cal2jd`
+"""
+function dat(dateVec::Vector{Float64})
+
+end
+
---
+++ test/Timing/JDate.jl
@@ -0,0 +1,14 @@
+@testset "JDate" begin
+    date1 = [2024, 5, 29, 8.0, 40, 30]
+    date2 = [1999.0, 1, 1, 0, 0, 0]
+
+    JD, MJD = dateVec2JDate(date1)
+    JD2, MJD2 = dateVec2JDate(date2)
+
+    @test isapprox(sum(JD), 2460459.8614598; atol=1e-5)
+    @test isapprox(sum(JD2[1]), 2451179.5000000; atol=1e-5)
+    @test JD2[2] == 0.0
+    @test MJD[2] == 0.0
+    @test MJD[1] == 51179.0
+
+end
