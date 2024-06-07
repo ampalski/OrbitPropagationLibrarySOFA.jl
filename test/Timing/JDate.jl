2895e76c37aafd7040b232127625f28a6a35f5e4
ampalski <ampalski@gmail.com>
2024-06-06 20:52:24 -0600

    fixing tests

--- test/Timing/JDate.jl
+++ test/Timing/JDate.jl
@@ -8,7 +8,7 @@
     @test isapprox(sum(JD), 2460459.8614598; atol=1e-5)
     @test isapprox(sum(JD2[1]), 2451179.5000000; atol=1e-5)
     @test JD2[2] == 0.0
-    @test MJD[2] == 0.0
-    @test MJD[1] == 51179.0
+    @test MJD2[2] == 0.0
+    @test MJD2[1] == 51179.0
 
 end
