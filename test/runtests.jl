e69b7bcf119d1e19905c26a2524d8e292f1065f2
Andrew Palski <ampalski@gmail.com>
2025-12-20 23:44:07 -0700

    more tests, more documentation

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,16 @@
 using OrbitPropagationLibrarySOFA
 using Test
 using StaticArrays
+using Aqua
+using JET
 
 const tests = [
     "Timing/JDate",
     "Timing/Conversions",
     "Timing/gmst",
     "Coordinates/rotations",
+    "Extras/aqua",
+    "Extras/jet",
 ]
 @testset "OrbitPropagationLibrarySOFA.jl" begin
     @testset "Test $t" for t in tests
